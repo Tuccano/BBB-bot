e0ac9636b2ae91d0da2bf42f3633fd1c0b141491
Irvig Krehan <IrvigKrehan@Hotmail.com>
2020-04-03 03:05:01 -0300

    'worker'

--- app.py
+++ app.py
@@ -1,72 +1,48 @@
-﻿from flask import Flask, Response
-import tweepy
+﻿import tweepy
 import os, sys
 from random import randint
-from threading import Thread
 import time
 
-
-app = Flask(__name__)
-
-isActive = True
-
 def xingar():
-    global isActive
+    consumer_key = "TLXPG47fXNoGCeErQc4dwpqiY"
+    consumer_secret = "GnGQzC62zmsh3vZPoEhyMWTe6LhCXVKRkPHPuITTyWFBR1WWJp"             # colocar os tokens etc
+    access_token = "1044965591562547200-131bSJ5ghM7TtGIWvo2Aw0xVduwLDA"
+    access_token_secret = "wqKG9nPZv1MiX4ee8Jj8qskwAnoFjuvWmaCecJe47Jeit"
 
-    while isActive:
-        consumer_key = ""
-        consumer_secret = ""             # colocar os tokens etc
-        access_token = ""
-        access_token_secret = ""
-
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-        api = tweepy.API(auth)   # login
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)   # login
 
 
-        pessoa_file = open("pessoas.txt", 'r')            
-        xingamento_file = open("xingamentos.txt", 'r')     # abre o arquivo
+    pessoa_file = open("pessoas.txt", 'r')            
+    xingamento_file = open("xingamentos.txt", 'r')     # abre o arquivo
 
-        pessoa = pessoa_file.readlines()
-        xingamento = xingamento_file.readlines()      # converte em lista
+    pessoa = pessoa_file.readlines()
+    xingamento = xingamento_file.readlines()      # converte em lista
 
-        pessoa_max = len(pessoa)
-        xingamento_max = len(xingamento)              # define o número de itens das listas
+    pessoa_max = len(pessoa)
+    xingamento_max = len(xingamento)              # define o número de itens das listas
 
-        random_pessoa = randint(0, pessoa_max) - 1
-        random_xingamento = randint(0, xingamento_max) - 1
+    random_pessoa = randint(0, pessoa_max) - 1
+    random_xingamento = randint(0, xingamento_max) - 1
 
-        texto = ""
+    texto = ""
+    texto2 = ""
+
+    if random_pessoa == 0:
+        texto = "babu lindo s2"
         texto2 = ""
+    else:
+        texto = f'vsf {pessoa[random_pessoa]}'
+        texto2 = f' sua {xingamento[random_xingamento]}'
 
-        if random_pessoa == 0:
-            texto = "babu lindo s2"
-            texto2 = ""
-        else:
-            texto = f'vsf {pessoa[random_pessoa]}'
-            texto2 = f' sua {xingamento[random_xingamento]}'
+    # print(random_pessoa)
+    # print(random_xingamento)
+    print(texto.strip('\n') + texto2)           # texto do post
 
-        # print(random_pessoa)
-        # print(random_xingamento)
-        # print(texto.strip('\n') + texto2)           # texto do post
+    # api.update_status(texto.strip('\n') + texto2)   # post final
 
-        api.update_status(texto.strip('\n') + texto2)   # post final
-
-        time.sleep(3600)
-
-def manual_run():
-    t = Thread(target=xingar)
-    t.start()
-    return "Xingando..."
-
-@app.route('/start', methods=['GET'])
-def start():
-    global isActive
-    isActive = True
-    return Response(manual_run(), mimetype="text/html")
-
-@app.route('/stop')
-def stop():
-    global isActive
-    isActive = False
-    return 'Stopped'
+while True:
+    xingar()
+    
+    time.sleep(2)
